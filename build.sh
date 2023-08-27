a198cf6f0d065ef09e5623c3d4bacab52c520c5f
AutoX <32790381+AoThen@users.noreply.github.com>
2023-08-27 20:22:53 +0800

    修复

--- build.sh
+++ build.sh
@@ -72,8 +72,10 @@ cat feeds.conf.default
 ./scripts/feeds install -a
 
 #  - luci-app-alist
+# rm -rf feeds/packages/lang/golang
+# svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+git clone  --depth 1 https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 
 echo CONFIG_ALL=y >.config
 make defconfig
